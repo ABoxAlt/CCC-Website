8410f4f5491e4274fe4b69e71fc82b64807c7dcb
ABoxAlt <1121133@apps.nsd.org>
2024-07-03 09:36:10 -0700

    More Mobile Compatability Updates

--- nav.js
+++ nav.js
@@ -1,15 +1,22 @@
 const navbar = document.querySelector(".navbar");
 const navImg = document.querySelector("#navImg")
-const widthText = document.querySelector("#widthText");
+
+const navButton = document.querySelector("#dropDownMenu")
 
 function navFunc() {
     if (window.innerWidth <= 910) {
         navbar.classList.add("disable");
         navImg.classList.add("navImgPadding");
+        
+        navButton.classList.remove("disable");
+        navButton.classList.add("enable");
     } else {
         if (navbar.classList.contains("disable")) {
             navbar.classList.remove("disable");
             navImg.classList.remove("navImgPadding");
+
+            navButton.classList.remove("enable")
+            navButton.classList.add("disable");
         }
     }
 }
